c3f2a50bb289064afc9c589387c652517ffce216
Robin Rainton <robin@rainton.com>
2020-11-09 15:57:18 +0100

    create-adapter stuff

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+    'google-smart-home-fulfillment adapter settings': {
+        'en': 'Adapter settings for google-smart-home-fulfillment',
+        'de': 'Adaptereinstellungen für google-smart-home-fulfillment',
+        'ru': 'Настройки адаптера для google-smart-home-fulfillment',
+        'pt': 'Configurações do adaptador para google-smart-home-fulfillment',
+        'nl': 'Adapterinstellingen voor google-smart-home-fulfillment',
+        'fr': "Paramètres d'adaptateur pour google-smart-home-fulfillment",
+        'it': "Impostazioni dell'adattatore per google-smart-home-fulfillment",
+        'es': 'Ajustes del adaptador para google-smart-home-fulfillment',
+        'pl': 'Ustawienia adaptera dla google-smart-home-fulfillment',
+        'zh-cn': 'google-smart-home-fulfillment的适配器设置'
+    },
+    'option1': {
+        'en': 'option1',
+        'de': 'Option 1',
+        'ru': 'Опция 1',
+        'pt': 'Opção 1',
+        'nl': 'Optie 1',
+        'fr': 'Option 1',
+        'it': 'opzione 1',
+        'es': 'Opción 1',
+        'pl': 'opcja 1',
+        'zh-cn': '选项1'
+    },
+    'option2': {
+        'en': 'option2',
+        'de': 'Option 2',
+        'ru': 'option2',
+        'pt': 'opção 2',
+        'nl': 'Optie 2',
+        'fr': 'Option 2',
+        'it': 'opzione 2',
+        'es': 'opcion 2',
+        'pl': 'Opcja 2',
+        'zh-cn': '选项2'
+    }
+};
