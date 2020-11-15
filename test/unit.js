e6d581936f6bc585d8d534d6ea348e0ff510b539
Robin Rainton <robin@rainton.com>
2020-11-15 10:25:30 +0100

    Dummy config for test

--- test/unit.js
+++ test/unit.js
@@ -2,4 +2,13 @@ const path = require('path');
 const { tests } = require('@iobroker/testing');
 
 // Run unit tests - See https://github.com/ioBroker/testing for a detailed explanation and further options
-tests.unit(path.join(__dirname, '..'));
+tests.unit(path.join(__dirname, '..'), {
+    // Add dummy config so the onReady doesn't fail deriving more
+    overwriteAdapterConfig(config) {
+        config = {
+            homeGraphJSONKey: '{"project_id": "dummy"}',
+            publicFQDN: "dummy"
+        };
+        return config;
+    }
+});
